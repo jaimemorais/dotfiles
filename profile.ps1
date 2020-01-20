a4a96b46982a135d69ebba81774e7051c080ae01
Jaime Morais <jaime.morais@serpro.gov.br>
2020-01-20 16:05:41 -0300

    Add profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,7 @@
+﻿
+#https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf
+#https://github.com/JanDeDobbeleer/oh-my-posh
+
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
