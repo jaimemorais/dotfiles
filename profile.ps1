97e8e52c64a399a077e8237c590aa2e07f1d367d
Jaime Morais <jaimemorais@gmail.com>
2020-01-26 14:22:11 -0300

    Add posh-docker

--- profile.ps1
+++ profile.ps1
@@ -2,6 +2,7 @@
 #https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf
 #https://github.com/JanDeDobbeleer/oh-my-posh
 
+Import-Module posh-docker
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
