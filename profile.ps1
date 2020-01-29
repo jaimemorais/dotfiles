f429b1a359e575bdd6c67e4b62e89efe34d3d199
Jaime Morais <jaimemorais@gmail.com>
2020-01-29 14:07:02 -0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,29 @@
-#Power Shell Profile
+#Power Shell Profile - Jaime
+
 #https://github.com/samneirinck/posh-docker
 #https://github.com/dahlbyk/posh-git
 #https://github.com/JanDeDobbeleer/oh-my-posh
 #https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf
 
+#modules
 Import-Module posh-docker
 Import-Module posh-git
 Import-Module oh-my-posh
+Set-Prompt
 Set-Theme Paradox
+
+#screen and buffer size
+$Shell = $Host.UI.RawUI
+$size = $Shell.WindowSize
+$size.width=175
+$size.height=45
+$Shell.WindowSize = $size
+$size = $Shell.BufferSize
+$size.width=175
+$size.height=5000
+$Shell.BufferSize = $size
+
+
+#init
+Set-Location C:\
+Clear-Host
