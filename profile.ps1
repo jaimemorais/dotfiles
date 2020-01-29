62b536974551d1b393c79bd8c843076bf497a729
Jaime Morais <jaimemorais@gmail.com>
2020-01-29 13:50:21 -0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,8 @@
-﻿
-#https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf
+#Power Shell Profile
+#https://github.com/samneirinck/posh-docker
+#https://github.com/dahlbyk/posh-git
 #https://github.com/JanDeDobbeleer/oh-my-posh
+#https://github.com/powerline/fonts/blob/master/DejaVuSansMono/DejaVu%20Sans%20Mono%20for%20Powerline.ttf
 
 Import-Module posh-docker
 Import-Module posh-git
