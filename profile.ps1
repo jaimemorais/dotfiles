f0e3d261ad2c65f0f662128be0714bd1fdb348b7
Jaime Morais <jaimemorais@gmail.com>
2021-07-14 15:40:48 -0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -2,7 +2,7 @@
 
 #Installing
 #Install-Module posh-git -Scope CurrentUser -Force
-#Install-Module posh-docker -Scope CurrentUser 
+#Install-Module posh-docker -Scope CurrentUser
 #Install-Module oh-my-posh -Scope CurrentUser -AllowPrerelease -Force
 #Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
 #Install-Module -Name Communary.ConsoleExtensions
@@ -10,16 +10,16 @@
 #Font : https://github.com/Znuff/consolas-powerline (Consolas NF)
 #Theme : https://github.com/jaimemorais/dotfiles/blob/master/JaimeTheme.psm1
 
-
 #Modules
 Import-Module posh-docker
 Import-Module posh-git
 Import-Module oh-my-posh
 Import-Module -Name Terminal-Icons
+
 #Set-Prompt
 Set-Theme JaimeTheme
 
-
+Set-PSReadLineOption -PredictionSource History
 
 
 #Error message colors
